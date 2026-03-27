ec14a27ad6fbdb6633aec124ff0bdcf479a2ea9e
CuiweiG <48gaocuiwei@gmail.com>
2026-03-28 07:32:20 +0800

    style: publication-quality plots (Okabe-Ito palette) + README gallery + pkgdown config

--- R/methods-plot.R
+++ R/methods-plot.R
@@ -51,49 +51,37 @@ autoplot.lfq_fit <- function(object,
 #' @export
 autoplot.lfq_forecast <- function(object, ...) {
   fc_start <- as.numeric(as.Date(attr(object, "forecast_start")))
+  lins     <- attr(object, "lineages")
 
-  p <- ggplot2::ggplot(object, ggplot2::aes(
-    x      = .data$.date,
-    y      = .data$.median,
-    colour = .data$.lineage,
-    fill   = .data$.lineage
+  ggplot2::ggplot(object, ggplot2::aes(
+    x = .data$.date, y = .data$.median,
+    colour = .data$.lineage, fill = .data$.lineage
   )) +
     ggplot2::geom_line(
-      data      = function(d) d[d$.type == "fitted", ],
+      data = function(d) d[d$.type == "fitted", ],
       linewidth = 0.8
     ) +
     ggplot2::geom_ribbon(
-      data    = function(d) d[d$.type == "forecast", ],
+      data = function(d) d[d$.type == "forecast", ],
       ggplot2::aes(ymin = .data$.lower, ymax = .data$.upper),
-      alpha   = 0.2,
-      colour  = NA
+      alpha = 0.2, colour = NA
     ) +
     ggplot2::geom_line(
-      data      = function(d) d[d$.type == "forecast", ],
-      linetype  = "dashed",
-      linewidth = 0.6
+      data = function(d) d[d$.type == "forecast", ],
+      linetype = "dashed", linewidth = 0.6
     ) +
     ggplot2::geom_vline(
-      xintercept = fc_start,
-      linetype   = "dotted",
-      colour     = "gray40"
+      xintercept = fc_start, linetype = "dotted", colour = "gray40"
     ) +
     ggplot2::scale_x_date(date_labels = "%Y-%m-%d") +
     ggplot2::scale_y_continuous(
-      labels = .pct_label,
-      limits = c(0, 1),
-      expand = c(0, 0)
+      labels = .pct_label, limits = c(0, 1), expand = c(0, 0)
     ) +
-    ggplot2::labs(
-      x      = NULL,
-      y      = "Frequency",
-      colour = "Lineage",
-      fill   = "Lineage"
-    ) +
-    ggplot2::theme_minimal(base_size = 12) +
-    ggplot2::theme(legend.position = "bottom")
-
-  p
+    .lfq_scale_colour(lins) +
+    .lfq_scale_fill(lins) +
+    ggplot2::labs(x = NULL, y = "Frequency",
+                  colour = "Lineage", fill = "Lineage") +
+    .lfq_theme()
 }
 
 
@@ -101,7 +89,8 @@ autoplot.lfq_forecast <- function(object, ...) {
 
 #' @noRd
 .plot_frequency <- function(fit) {
-  df <- fit$residuals
+  df   <- fit$residuals
+  lins <- fit$lineages
 
   ggplot2::ggplot(df, ggplot2::aes(x = .data$.date)) +
     ggplot2::geom_line(
@@ -110,22 +99,15 @@ autoplot.lfq_forecast <- function(object, ...) {
     ) +
     ggplot2::geom_point(
       ggplot2::aes(y = .data$.observed, colour = .data$.lineage),
-      size  = 1.2,
-      alpha = 0.6
-    ) +
-    ggplot2::scale_y_continuous(
-      labels = .pct_label,
-      limits = c(0, 1),
-      expand = c(0, 0)
+      size = 1.5, alpha = 0.5
     ) +
     ggplot2::scale_x_date(date_labels = "%Y-%m-%d") +
-    ggplot2::labs(
-      x      = NULL,
-      y      = "Frequency",
-      colour = "Lineage"
+    ggplot2::scale_y_continuous(
+      labels = .pct_label, limits = c(0, 1), expand = c(0, 0)
     ) +
-    ggplot2::theme_minimal(base_size = 12) +
-    ggplot2::theme(legend.position = "bottom")
+    .lfq_scale_colour(lins) +
+    ggplot2::labs(x = NULL, y = "Frequency", colour = "Lineage") +
+    .lfq_theme()
 }
 
 
@@ -137,68 +119,73 @@ autoplot.lfq_forecast <- function(object, ...) {
     )
   }
 
-  ga <- growth_advantage(fit, type = "relative_Rt",
-                         generation_time = generation_time)
-  ga <- ga[ga$lineage != fit$pivot, ]
+  ga   <- growth_advantage(fit, type = "relative_Rt",
+                           generation_time = generation_time)
+  ga   <- ga[ga$lineage != fit$pivot, ]
+  lins <- ga$lineage
 
   ggplot2::ggplot(ga, ggplot2::aes(
     x    = .data$estimate,
     y    = stats::reorder(.data$lineage, .data$estimate),
-    xmin = .data$lower,
-    xmax = .data$upper
+    xmin = .data$lower, xmax = .data$upper,
+    colour = .data$lineage
   )) +
     ggplot2::geom_vline(xintercept = 1, linetype = "dashed",
                         colour = "gray50") +
-    ggplot2::geom_pointrange(linewidth = 0.5) +
+    ggplot2::geom_pointrange(linewidth = 0.6, size = 0.8) +
+    .lfq_scale_colour(lins) +
     ggplot2::labs(
       x       = paste0("Relative Rt (gen. time = ",
                        generation_time, " days)"),
       y       = NULL,
       caption = paste("Pivot:", fit$pivot)
     ) +
-    ggplot2::theme_minimal(base_size = 12)
+    .lfq_theme() +
+    ggplot2::theme(legend.position = "none")
 }
 
 
 #' @noRd
 .plot_trajectory <- function(fit) {
-  df <- fit$residuals
+  df   <- fit$residuals
+  lins <- fit$lineages
 
   ggplot2::ggplot(df, ggplot2::aes(x = .data$.date)) +
     ggplot2::geom_point(
-      ggplot2::aes(y = .data$.observed),
-      alpha = 0.5,
-      size  = 1.5
+      ggplot2::aes(y = .data$.observed, colour = .data$.lineage),
+      alpha = 0.5, size = 1.5
     ) +
     ggplot2::geom_line(
-      ggplot2::aes(y = .data$.fitted_freq),
+      ggplot2::aes(y = .data$.fitted_freq, colour = .data$.lineage),
       linewidth = 0.8
     ) +
     ggplot2::facet_wrap(~ .data$.lineage, scales = "free_y") +
-    ggplot2::scale_x_date(date_labels = "%Y-%m-%d") +
+    ggplot2::scale_x_date(date_labels = "%Y-%m") +
     ggplot2::scale_y_continuous(labels = .pct_label) +
+    .lfq_scale_colour(lins) +
     ggplot2::labs(x = NULL, y = "Frequency") +
-    ggplot2::theme_minimal(base_size = 11)
+    .lfq_theme(base_size = 11) +
+    ggplot2::theme(legend.position = "none")
 }
 
 
 #' @noRd
 .plot_residuals <- function(fit) {
-  df <- fit$residuals
-  df <- df[is.finite(df$.pearson_resid), ]
+  df   <- fit$residuals
+  df   <- df[is.finite(df$.pearson_resid), ]
+  lins <- fit$lineages
 
   ggplot2::ggplot(df, ggplot2::aes(
-    x      = .data$.date,
-    y      = .data$.pearson_resid,
+    x = .data$.date, y = .data$.pearson_resid,
     colour = .data$.lineage
   )) +
     ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +
-    ggplot2::geom_point(alpha = 0.6) +
+    ggplot2::geom_point(alpha = 0.6, size = 1.5) +
     ggplot2::scale_x_date(date_labels = "%Y-%m-%d") +
+    .lfq_scale_colour(lins) +
     ggplot2::labs(x = NULL, y = "Pearson residual",
                   colour = "Lineage") +
-    ggplot2::theme_minimal(base_size = 12) +
-    ggplot2::theme(legend.position = "bottom")
+    .lfq_theme()
 }
 
 
